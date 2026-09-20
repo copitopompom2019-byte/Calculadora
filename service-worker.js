63c5bc7b87d5efc237864212349b662e01596d95
copitopompom2019-byte <copitopompom2019@gmail.com>
2026-09-19 21:01:19 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "calculadora-bcv-v3";
+const CACHE_NAME = "calculadora-bcv-v4";
 
-const FILES_TO_CACHE = [
+const APP_FILES = [
     "./",
     "./index.html",
     "./style.css",
@@ -15,98 +15,99 @@ const FILES_TO_CACHE = [
 // INSTALACIÓN
 // ========================================
 
-self.addEventListener("install", event => {
+self.addEventListener(
+    "install",
+    event => {
 
-    event.waitUntil(
+        event.waitUntil(
+            caches
+                .open(CACHE_NAME)
+                .then(cache => {
+                    return cache.addAll(APP_FILES);
+                })
+        );
 
-        caches.open(CACHE_NAME)
-            .then(cache => {
-
-                return cache.addAll(
-                    FILES_TO_CACHE
-                );
-
-            })
-
-    );
-
-    self.skipWaiting();
-});
+        self.skipWaiting();
+    }
+);
 
 
 // ========================================
 // ACTIVACIÓN
 // ========================================
 
-self.addEventListener("activate", event => {
+self.addEventListener(
+    "activate",
+    event => {
 
-    event.waitUntil(
+        event.waitUntil(
+            caches
+                .keys()
+                .then(cacheNames => {
+                    return Promise.all(
+                        cacheNames.map(cacheName => {
 
-        caches.keys()
-            .then(cacheNames => {
-
-                return Promise.all(
-
-                    cacheNames
-                        .filter(
-                            cacheName =>
+                            if (
                                 cacheName !== CACHE_NAME
-                        )
-                        .map(
-                            cacheName =>
-                                caches.delete(cacheName)
-                        )
+                            ) {
+                                return caches.delete(
+                                    cacheName
+                                );
+                            }
 
-                );
-
-            })
-
-    );
-
-    self.clients.claim();
-});
-
-
-// ========================================
-// PETICIONES
-// ========================================
-
-self.addEventListener("fetch", event => {
-
-    const request = event.request;
-
-    // Las consultas de las APIs siempre
-    // deben ir directamente a Internet.
-    if (
-        request.url.includes("bcv.today") ||
-        request.url.includes("workers.dev")
-    ) {
-        event.respondWith(
-            fetch(request)
+                        })
+                    );
+                })
         );
 
-        return;
+        self.clients.claim();
     }
+);
 
 
-    event.respondWith(
+// ========================================
+// FETCH
+// ========================================
 
-        caches.match(request)
-            .then(cachedResponse => {
+self.addEventListener(
+    "fetch",
+    event => {
 
-                if (cachedResponse) {
+        if (
+            event.request.method !== "GET"
+        ) {
+            return;
+        }
 
-                    return cachedResponse;
-                }
+        event.respondWith(
 
-                return fetch(request)
-                    .then(response => {
+            fetch(event.request)
 
-                        return response;
-                    });
+                .then(response => {
 
-            })
+                    const responseClone =
+                        response.clone();
 
-    );
+                    caches
+                        .open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(
+                                event.request,
+                                responseClone
+                            );
+                        });
 
-});
+                    return response;
+
+                })
+
+                .catch(() => {
+                    return caches.match(
+                        event.request
+                    );
+                })
+
+        );
+
+    }
+);
